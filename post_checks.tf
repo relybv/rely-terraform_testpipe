7ed19666a0eee4aaec0f36e43eabf82e8e7ee291
P. Gomersbach <p.gomersbach@rely.nl>
2017-04-12 15:55:15 +0000

    add post checks

--- post_checks.tf
+++ post_checks.tf
@@ -5,14 +5,32 @@ resource "null_resource" "post_checks_lb1" {
   connection {
     host = "${openstack_compute_floatingip_v2.lb.address}"
     user = "${var.lb_username}"
-    private_key = "${file(var.ssh_key_file)}"
+    private_key = "${var.ssh_key_file}"
   }
   provisioner "remote-exec" {
     script = "wait_provision.sh"
   }
   provisioner "remote-exec" {
     inline = [
-      "# sudo /opt/puppetlabs/puppet/bin/rspec /usr/local/rspec_tests/ -f d"
+      "sudo /opt/puppetlabs/puppet/bin/rspec /usr/local/rspec_tests/ -f d"
+    ]
+  }
+}
+resource "null_resource" "post_checks_monitor1" {
+  triggers {
+    cluster_instance_ids = "${openstack_compute_instance_v2.monitor1.id}"
+  }
+  connection {
+    host = "${openstack_compute_floatingip_v2.monitor.address}"
+    user = "${var.monitor_username}"
+    private_key = "${var.ssh_key_file}"
+  }
+  provisioner "remote-exec" {
+    script = "wait_provision.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo /opt/puppetlabs/puppet/bin/rspec /usr/local/rspec_tests/ -f d"
     ]
   }
 }
@@ -24,17 +42,17 @@ resource "null_resource" "post_checks_appl1" {
   connection {
     bastion_host = "${openstack_compute_floatingip_v2.monitor.address}"
     bastion_user = "${var.monitor_username}"
-    bastion_private_key = "${file(var.ssh_key_file)}"
+    bastion_private_key = "${var.ssh_key_file}"
     host = "${var.appl1_ip_address}"
     user = "${var.appl_username}"
-    private_key = "${file(var.ssh_key_file)}"
+    private_key = "${var.ssh_key_file}"
   }
   provisioner "remote-exec" {
     script = "wait_provision.sh"
   }
   provisioner "remote-exec" {
     inline = [
-      "# sudo /opt/puppetlabs/puppet/bin/rspec /usr/local/rspec_tests/ -f d"
+      "sudo /opt/puppetlabs/puppet/bin/rspec /usr/local/rspec_tests/ -f d"
     ]
   }
 }
@@ -46,17 +64,39 @@ resource "null_resource" "post_checks_appl2" {
   connection {
     bastion_host = "${openstack_compute_floatingip_v2.monitor.address}"
     bastion_user = "${var.monitor_username}"
-    bastion_private_key = "${file(var.ssh_key_file)}"
+    bastion_private_key = "${var.ssh_key_file}"
     host = "${var.appl2_ip_address}"
     user = "${var.appl_username}"
-    private_key = "${file(var.ssh_key_file)}"
+    private_key = "${var.ssh_key_file}"
   }
   provisioner "remote-exec" {
     script = "wait_provision.sh"
   }
   provisioner "remote-exec" {
     inline = [
-      "# sudo /opt/puppetlabs/puppet/bin/rspec /usr/local/rspec_tests/ -f d"
+      "sudo /opt/puppetlabs/puppet/bin/rspec /usr/local/rspec_tests/ -f d"
+    ]
+  }
+}
+
+resource "null_resource" "post_checks_db1" {
+  triggers {
+    cluster_instance_ids = "${openstack_compute_instance_v2.db1.id}"
+  }
+  connection {
+    bastion_host = "${openstack_compute_floatingip_v2.monitor.address}"
+    bastion_user = "${var.monitor_username}"
+    bastion_private_key = "${var.ssh_key_file}"
+    host = "${var.db1_ip_address}"
+    user = "${var.db_username}"
+    private_key = "${var.ssh_key_file}"
+  }
+  provisioner "remote-exec" {
+    script = "wait_provision.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo /opt/puppetlabs/puppet/bin/rspec /usr/local/rspec_tests/ -f d"
     ]
   }
 }
