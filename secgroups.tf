125611fe119fee5a9f7089813486e9f96152886a
P. Gomersbach <p.gomersbach@rely.nl>
2017-04-20 09:01:16 +0000

    add grafana port

--- secgroups.tf
+++ secgroups.tf
@@ -130,6 +130,12 @@ resource "openstack_compute_secgroup_v2" "monitor" {
   region = "${var.region}"
   description = "Security group for the Monitor instances"
   rule {
+    from_port = 8080
+    to_port = 8080
+    ip_protocol = "tcp"
+    cidr = "0.0.0.0/0"
+  }
+  rule {
     from_port = 22
     to_port = 22
     ip_protocol = "tcp"
